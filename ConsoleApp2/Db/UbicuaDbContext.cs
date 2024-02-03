41e6a31237b4a923e6ed4736c104d59917aafd56
EstebanEr-03 <esteban.enriquezr11@gmail.com>
2024-02-02 19:04:55 -0500

    Se agrega proyecto funcionando base de datos y sensor DHTT22

---
+++ ConsoleApp2/Db/UbicuaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2.Db
+{
+    public class UbicuaDbContext : DbContext
+    {
+        private const string _connection = "Server=ESTEBAN\\SQLEXPRESS;Initial Catalog=ProuebaPractica;User ID=sa;Password=gestion01;TrustServerCertificate=True;";
+        public DbSet<Dato> Datos { get; set; } // Reemplaza TuEntidad y TuTabla con nombres apropiados
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connection);
+        }
+    }
+}
